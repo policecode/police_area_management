1cc7e35c7af6d48bc6fffd4711d8f487404c3104
Nguyễn Hoàng Đạt <dat01655993202@gmail.com>
2025-03-12 23:24:30 +0700

    'update'

--- resources/views/parts/ads/adsense_popunder.blade.php
+++ resources/views/parts/ads/adsense_popunder.blade.php
@@ -1,2 +1,2 @@
 {{-- Adsterra --}}
-{{-- <script type='text/javascript' src='//pl25163510.profitablecpmrate.com/5c/d2/23/5cd2239bbaecdbd0bfc371618859c562.js'></script> --}}
+<script type='text/javascript' src='//pl25163510.effectiveratecpm.com/5c/d2/23/5cd2239bbaecdbd0bfc371618859c562.js'></script>
--- resources/views/parts/ads/adsense_social.blade.php
+++ resources/views/parts/ads/adsense_social.blade.php
@@ -1,2 +1,2 @@
 {{-- Adsterra --}}
-<script type='text/javascript' src='//pl25181817.profitablecpmrate.com/46/39/ef/4639ef76168d4a5214afe6f41273adc9.js'></script>
+<script type='text/javascript' src='//pl25181817.effectiveratecpm.com/46/39/ef/4639ef76168d4a5214afe6f41273adc9.js'></script>
