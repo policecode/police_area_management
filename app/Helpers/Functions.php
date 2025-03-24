a20e77ccc98cd85aa9674daefd14a39ffd727d89
Nguyễn Hoàng Đạt <dat01655993202@gmail.com>
2025-03-24 22:51:06 +0700

    fix version

--- app/Helpers/Functions.php
+++ app/Helpers/Functions.php
@@ -3,7 +3,7 @@
 use App\Models\Category;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Cookie;
-define('FVN_VERSION_LARAVEL', '1.0.3');
+define('FVN_VERSION_LARAVEL', '1.0.4');
 
 function get_all_categories($type = 1) {
     $categories = [];
