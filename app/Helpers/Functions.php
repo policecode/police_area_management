f53f90c8d737449a85279affa45e4193eba6571d
Nguyễn Hoàng Đạt <dat01655993202@gmail.com>
2025-12-30 23:12:29 +0700

    'fix'

--- app/Helpers/Functions.php
+++ app/Helpers/Functions.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
-define('FVN_VERSION_LARAVEL', '1.0.5');
+define('FVN_VERSION_LARAVEL', '1.0.6');
 
 function get_all_categories($type = 1) {
     $categories = [];
