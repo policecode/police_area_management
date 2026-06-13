3ed0b852fbd16db9eb65421054e6ed1dfb301140
Nguyễn Hoàng Đạt <dat01655993202@gmail.com>
2026-06-13 23:49:23 +0700

    'update'

--- app/Http/Controllers/Member/AudioController.php
+++ app/Http/Controllers/Member/AudioController.php
@@ -11,14 +11,24 @@ use Symfony\Component\Process\Exception\ProcessFailedException;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\File;
+use Symfony\Component\Process\ExecutableFinder;
 
 class AudioController extends Controller
 {
     public function generateAudio(Request $request)
     {
+        // - Khởi tạo bộ tìm kiếm file thực thi của hệ thống
+        $executableFinder = new ExecutableFinder();
+        // - Ưu tiên tìm lệnh 'python3' trước, nếu không thấy thì tìm lệnh 'python'
+        $pythonBinary = $executableFinder->find('python3') ?? $executableFinder->find('python');
+
+        // - Phòng trường hợp server hoàn toàn chưa cài Python
+        if (!$pythonBinary) {
+            throw new \Exception('Hệ thống chưa cài đặt Python hoặc chưa cấu hình biến môi trường (Environment Path).');
+        }
         // 1. Lấy text từ client gửi lên (hoặc lấy từ database truyện của bạn)
-        $story = Story::find(218);
-        $chapters = Chaper::getByStory(218)->GetByPosition(659)->first();
+        $story = Story::GetBySlug('cau-tai-vo-dao-the-gioi-thanh-thanh')->first();
+        $chapters = Chaper::getByStory($story->id)->GetByPosition(1)->first();
         $content = $chapters->content;
         $text = handleFixSpellingErrors($content);
         $text = strip_tags($text);
@@ -29,14 +39,18 @@ class AudioController extends Controller
         //     'message' =>  $text
         // ]);
         // 2. Tạo tên file audio và file text tạm
+        if (!Storage::disk('public')->exists('audio')) {
+            Storage::disk('public')->makeDirectory('audio');
+        }
         $audioName = 'tmpAudio.mp3';
         $textTempName = 'tmpAudio.txt';
 
         $absoluteOutputPath = storage_path('app/public/audio/' . $audioName);
-
+        if (File::exists($absoluteOutputPath)) {
+            File::delete($absoluteOutputPath);
+        }
         // Đường dẫn file text tạm để truyền cho Python đọc
         $absoluteTextPath = storage_path('app/public/audio/' . $textTempName);
-
         // 3. Ghi dữ liệu text vào file tạm (Xử lý được mọi loại dấu ngoặc kép, xuống dòng)
         File::put($absoluteTextPath, $text);
 
@@ -44,7 +58,10 @@ class AudioController extends Controller
         $scriptPath = storage_path('app/scripts/tts.py');
 
         // 5. Khởi tạo Process: Bây giờ ta truyền ĐƯỜNG DẪN FILE TEXT chứ không truyền chuỗi text nữa
-        $process = new Process(['python', $scriptPath, $absoluteTextPath, $absoluteOutputPath]);
+
+        // $process = new Process(['python', $scriptPath, $absoluteTextPath, $absoluteOutputPath]); //trên local
+        $process = new Process([$pythonBinary, $scriptPath, $absoluteTextPath, $absoluteOutputPath]); //trên server
+
         $process->setTimeout(null); // Tắt timeout
 
         // 6. Kích hoạt chạy
@@ -61,6 +78,14 @@ class AudioController extends Controller
             // $errorOutput = $process->getErrorOutput();
             // dd($errorOutput); // Dừng hệ thống và in ra lỗi chi tiết
         }
+        $audioUrl = Storage::url('audio/' . $audioName);
+
+        // Trả về dữ liệu JSON cho client gọi Ajax/Fetch trên giao diện nhận được
+        return response()->json([
+            'success' => true,
+            'message' => 'Tạo âm thanh thành công!',
+            'audio_url' => asset($audioUrl) // Trả về link tuyệt đối: http://domain.com/storage/audio/xxx.mp3
+        ]);
 
         // ================= TIẾN TRÌNH ĐẨY LÊN CLOUD R2 =================
         $r2AudioUrl = null;
@@ -70,7 +95,7 @@ class AudioController extends Controller
             $fileStream = fopen($absoluteOutputPath, 'r+');
 
             // 2. Đường dẫn thư mục bạn muốn lưu trên Cloudflare R2 (Ví dụ: danh_muc_audio/file.mp3)
-            $r2Path = 'audio-stories/'.$story->slug . '/' . $audioName;
+            $r2Path = 'audio-stories/' . $story->slug . '/' . $audioName;
 
             // 3. Đẩy tự động lên Cloudflare R2, cấu hình quyền public-read để client bật link nghe được luôn
             Storage::disk('r2')->put($r2Path, $fileStream, [
