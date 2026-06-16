6d719981e9e1016afb350251dd695151793f95a2
Nguyễn Hoàng Đạt <dat01655993202@gmail.com>
2026-06-16 12:10:28 +0700

    'update'

--- app/Helpers/Functions.php
+++ app/Helpers/Functions.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
-define('FVN_VERSION_LARAVEL', '1.0.6');
+define('FVN_VERSION_LARAVEL', '1.0.7');
 
 function get_all_categories($type = 1)
 {
